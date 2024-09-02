d484138be6227162abf856fc4810db5b54b9ae8d
Pavan Gandham <77606826+PavanGandham@users.noreply.github.com>
2024-09-02 09:13:05 +0530

    python programs

--- integer-multiples.py
+++
@@ -1,7 +0,0 @@
-# Python Program to check whether the given integer is multiple of 5 and 7 
-
-number = int(input("Enter the integer: "))
-if((number%5==0 and number%7==0)):
-    print("The nunber is multiple of both 5 and 7")
-else:
-    print("The number is not a multiple of both 5 and 7")
